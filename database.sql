9296eeb55626890a1320c6b0e8cb78486a7e979a
shaylahong <seihaleak.hong@gmail.com>
2023-10-05 21:25:22 +1030

    add data cleanup and SQL database

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Disasters(
+ Year INT,
+ Disaster_Type VARCHAR(255),
+ Disaster_Subtype VARCHAR(255),
+ Country VARCHAR(255),
+ Total_Affected FLOAT,
+ Total_Damages FLOAT
+ );
+ 
+ select * from Disasters
